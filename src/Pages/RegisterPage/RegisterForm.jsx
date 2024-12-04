dc60bc78edef5f204adb7af0a2ad016a562a09f8
Sharif-Miah <farhansharif1410@gmail.com>
2024-12-04 23:27:19 +0600

    register design done

---
+++ src/Pages/RegisterPage/RegisterForm.jsx
@@ -0,0 +1,117 @@
+/* eslint-disable no-extra-boolean-cast */
+import { useForm } from 'react-hook-form';
+import Field from '../../component/common/Field/Field';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+
+const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const navigate = useNavigate();
+  const toistfy = () => toast.success('Successfully Register!');
+
+  const handleForm = async (formData) => {
+    console.log(formData);
+    try {
+      let response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/register`,
+        formData
+      );
+
+      if (response.status === 201) {
+        navigate('/login');
+        toistfy();
+      }
+    } catch (error) {
+      console.error(error);
+      setError('root.random', {
+        type: 'random',
+        message: `Something went wrong: ${error.message}`,
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleForm)}>
+      <Field
+        error={errors.firstName}
+        label='First Name'>
+        <input
+          {...register('firstName', { required: 'First Name is requerd' })}
+          type='firstName'
+          name='firstName'
+          id='firstName'
+          placeholder='FirstName'
+          className={`auth-input ${
+            !!errors.email ? 'border-red-500' : 'border-gray-200'
+          } `}
+        />
+      </Field>
+      <Field
+        error={errors.lastName}
+        label='LastName'>
+        <input
+          {...register('lastName')}
+          type='lastName'
+          name='lastName'
+          id='lastName'
+          placeholder='LastName'
+          className='auth-input'
+        />
+      </Field>
+      <Field
+        error={errors.email}
+        label='email'>
+        <input
+          {...register('email', { required: 'Email is requerd' })}
+          type='email'
+          name='email'
+          id='email'
+          placeholder='Email'
+          className={`auth-input ${
+            !!errors.email ? 'border-red-500' : 'border-gray-200'
+          } `}
+        />
+      </Field>
+
+      <Field
+        error={errors.password}
+        label='password'>
+        <input
+          {...register('password', {
+            required: 'password is requerd',
+            minLength: {
+              value: 8,
+              message: 'Your password must be at least 8 characters',
+            },
+          })}
+          type='password'
+          name='password'
+          id='password'
+          placeholder='password'
+          className={`auth-input ${
+            !!errors.password ? 'border-red-500' : 'border-gray-200'
+          } `}
+        />
+      </Field>
+      <div>
+        <p className='my-2 text-red-500'>{errors?.root?.random?.message}</p>
+      </div>
+      <Field>
+        <button
+          className='auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90'
+          type='submit'>
+          Register
+        </button>
+      </Field>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,45 @@
+import { Link } from 'react-router';
+import RegisterForm from './RegisterForm';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <main className='flex min-h-screen items-center justify-center bg-deepDark py-8'>
+      <div className='max-w-[1368px] flex-1'>
+        <div className='container grid items-center gap-8 lg:grid-cols-2'>
+          <div>
+            <img
+              className='mb-12 h-60'
+              src='./assets/icons/registration.svg'
+              alt='auth_illustration'
+            />
+            <div>
+              <h1 className='mb-3 text-4xl font-bold lg:text-[40px]'>
+                Facehook
+              </h1>
+              <p className='max-w-[452px] text-gray-400/95 lg:text-lg'>
+                Create a social media app with features like, showing the post,
+                post details, reactions, comments and profile.
+              </p>
+            </div>
+          </div>
+          <div className='card'>
+            <h1 className='my-4 text-2xl font-bold text-center'>Register</h1>
+            <RegisterForm />
+            <div className='py-4 lg:py-6'>
+              <p className='text-center text-xs text-gray-600/95 lg:text-sm'>
+                Already have an account?{' '}
+                <Link
+                  to='/login'
+                  className='text-white transition-all hover:text-lwsGreen hover:underline'>
+                  Login Now
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default RegisterPage;
