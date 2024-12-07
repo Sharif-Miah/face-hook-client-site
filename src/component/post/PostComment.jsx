f88276f857b8dd92f4a97f13ea55ec504745ffbb
Sharif-Miah <farhansharif1410@gmail.com>
2024-12-07 20:44:17 +0600

    comment code adde

--- src/component/post/PostComment.jsx
+++ src/component/post/PostComment.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react';
 import PostCommentsList from './PostCommentsList';
 import { useAvatar } from '../../hook/useAvatar';
+import useAxios from '../../hook/useAxios';
+import useAuth from '../../hook/useAuth';
 
 const PostComment = ({ post }) => {
   const [commentShow, setCommentShow] = useState(false);
-  const { avatarURL } = useAvatar(post);
+  const { auth } = useAuth();
+  const [comments, setComments] = useState(post?.comments);
+  const [comment, setComment] = useState('');
+  const { api } = useAxios();
 
   const handleShowComment = () => {
     setCommentShow(!commentShow);
+  };
+
+  const addComment = async (event) => {
+    const keyCode = event.keyCode;
+
+    if (keyCode === 13) {
+      try {
+        const response = await api.patch(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/comment`,
+          { comment }
+        );
+
+        if (response.status === 200) {
+          setComments([...response.data.comments]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
 
   return (
@@ -15,7 +39,7 @@ const PostComment = ({ post }) => {
       <div className='flex-center mb-3 gap-2 lg:gap-4'>
         <img
           className='max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]'
-          src={avatarURL}
+          src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
           alt='avatar'
         />
 
@@ -25,6 +49,9 @@ const PostComment = ({ post }) => {
             className='h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]'
             name='post'
             id='post'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            onKeyDown={(e) => addComment(e)}
             placeholder="What's on your mind?"
           />
         </div>
@@ -36,7 +63,7 @@ const PostComment = ({ post }) => {
           All Comment ▾
         </button>
       </div>
-      {commentShow && <PostCommentsList comments={post?.comments} />}
+      {commentShow && <PostCommentsList comments={comments} />}
     </div>
   );
 };
